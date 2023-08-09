1256e9c539b2430e5bcec95aa4bd62d1563cde8e
Alessandro Lioi <lioialessandro2000@live.com>
2023-08-09 18:42:22 +0200

    feat(pagerank): implemented the algorithm using goroutines
    This implementation is he foundation of the distributed implementation
    The goroutines are used to determine what messages the working nodes
    have to send to each other

--- lib/pagerank.go
+++ lib/pagerank.go
@@ -31,3 +31,48 @@ func (g *Graph) PageRank(dampingFactor, threshold float64) {
 		}
 	}
 }
+
+func (g *Graph) GoroutinesPageRank(dampingFactor, threshold float64) {
+	contributionChannel := make(chan map[int]float64)
+	convergenceChannel := make(chan float64)
+	for i := 0; i < 100; i++ {
+		sum := make(map[int]float64)
+		// Map
+		for _, u := range g.Nodes {
+			go mapper(u, contributionChannel)
+			contributions := <-contributionChannel
+			for id := range contributions {
+				sum[id] += contributions[id]
+			}
+		}
+
+		// Reduce
+		convergenceDiff := 0.0
+		for id, node := range g.Nodes {
+			go reducer(node, sum[id], dampingFactor, convergenceChannel)
+			convergenceDiff += <-convergenceChannel
+		}
+		if convergenceDiff < threshold {
+			fmt.Printf("Converged after %d iteration(s)\n", i+1)
+			break
+		}
+	}
+}
+
+func mapper(u *Node, channel chan map[int]float64) {
+	contributions := make(map[int]float64)
+	nV := float64(len(u.OutLinks))
+	for _, v := range u.OutLinks {
+		contributions[v.ID] = u.Rank / nV
+	}
+	channel <- contributions
+}
+
+func reducer(node *Node, sum, dampingFactor float64,
+	convergenceDiff chan float64) {
+	oldRank := node.Rank
+	newRank := dampingFactor*sum + (1-dampingFactor)*node.EValue
+	diff := math.Abs(newRank - oldRank)
+	node.Rank = newRank
+	convergenceDiff <- diff
+}
--- main.go
+++ main.go
@@ -9,10 +9,19 @@ func main() {
 	dampingFactor := 0.85
 	threshold := 0.0001
 
+	println("Iterative Approach")
 	var graph lib.Graph
 	if err := graph.LoadFromFile("graph.txt"); err != nil {
 		panic(err)
 	}
 	graph.PageRank(dampingFactor, threshold)
 	graph.Print()
+
+	println("Goroutines Approach")
+	var goGraph lib.Graph
+	if err := goGraph.LoadFromFile("graph.txt"); err != nil {
+		panic(err)
+	}
+	goGraph.GoroutinesPageRank(dampingFactor, threshold)
+	goGraph.Print()
 }
