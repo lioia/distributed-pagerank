c4b63f5f40dc54ee2428b6b5935c221c2ec56163
Alessandro Lioi <lioialessandro2000@live.com>
2023-09-01 16:07:59 +0200

    feat(general): rename NODE_PORT to PORT; fix compose configuration

--- main.go
+++ main.go
@@ -21,17 +21,20 @@ import (
 )
 
 func main() {
+	// Read environment variables
 	master, err := utils.ReadStringEnvVar("MASTER")
 	utils.FailOnError("Failed to read environment variables", err)
 	rabbitHost, err := utils.ReadStringEnvVar("RABBIT_HOST")
 	utils.FailOnError("Failed to read environment variables", err)
 	rabbitUser := utils.ReadStringEnvVarOr("RABBIT_USER", "guest")
 	rabbitPass := utils.ReadStringEnvVarOr("RABBIT_PASSWORD", "guest")
-	nodePort, err := utils.ReadIntEnvVar("NODE_PORT")
+	port, err := utils.ReadIntEnvVar("PORT")
 	utils.FailOnError("Failed to read environment variables", err)
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", nodePort))
+	// Create connection
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	utils.FailOnError("Failed to listen for node server", err)
+	// lis.Close in goroutine
 
 	// Connect to RabbitMQ
 	queue := fmt.Sprintf("amqp://%s:%s@%s:5672/", rabbitUser, rabbitPass, rabbitHost)
@@ -42,6 +45,7 @@ func main() {
 	utils.FailOnError("Failed to open a channel to RabbitMQ", err)
 	defer ch.Close()
 
+	// Base node values
 	n := node.Node{
 		State: &proto.State{
 			Phase: int32(node.Wait),
@@ -53,6 +57,7 @@ func main() {
 			Channel: ch,
 		},
 	}
+	// Default value for master
 	workQueueName := "work"
 	resultQueueName := "result"
 
@@ -87,6 +92,7 @@ func main() {
 		workQueueName = join.WorkQueue
 		resultQueueName = join.ResultQueue
 	}
+	// Queue declaration
 	work, err := utils.DeclareQueue(workQueueName, ch)
 	utils.FailOnError("Failed to declare 'work' queue", err)
 	n.Queue.Work = &work
@@ -97,6 +103,7 @@ func main() {
 	// Running gRPC server for internal network communication in a goroutine
 	go func() {
 		// Creating gRPC server
+		defer lis.Close()
 		server := grpc.NewServer()
 		proto.RegisterNodeServer(server, &node.NodeServerImpl{Node: &n})
 		log.Printf("Starting %s node at %v\n", node.RoleToString(n.Role), lis.Addr())
